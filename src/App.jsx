c9843fd0282afeab9c4933c7d3ed47f677325a9d
karlito0oo <73117222+karlito0oo@users.noreply.github.com>
2025-08-30 20:49:45 +0800

    articles section

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Login from './pages/Login';
-import Admin from './pages/Admin';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-import Hero from './components/Hero';
-import FeaturedProducts from './components/FeaturedProducts';
-import Testimonials from './components/Testimonials';
-import AboutNoel from './components/AboutNoel';
-import Products from './pages/Products';
-import Contact from './pages/Contact';
-import ProductDetails from './pages/ProductDetails';
-import './styles/featured-products.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Admin from "./pages/Admin";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Hero from "./components/Hero";
+import FeaturedProducts from "./components/FeaturedProducts";
+import Testimonials from "./components/Testimonials";
+import Articles from "./components/Articles";
+import ArticleDetails from "./pages/ArticleDetails";
+import AboutNoel from "./components/AboutNoel";
+import Products from "./pages/Products";
+import Contact from "./pages/Contact";
+import ProductDetails from "./pages/ProductDetails";
+import "./styles/featured-products.css";
 
 function App() {
   return (
@@ -19,19 +21,24 @@ function App() {
         <Navbar />
         <main className="flex-grow">
           <Routes>
-            <Route path="/" element={
-              <>
-                <Hero />
-                <AboutNoel />
-                <FeaturedProducts />
-                <Testimonials />
-              </>
-            } />
+            <Route
+              path="/"
+              element={
+                <>
+                  <Hero />
+                  <AboutNoel />
+                  <FeaturedProducts />
+                  <Articles />
+                  <Testimonials />
+                </>
+              }
+            />
             <Route path="/products" element={<Products />} />
             <Route path="/products/:id/:slug" element={<ProductDetails />} />
             <Route path="/contact" element={<Contact />} />
             <Route path="/login" element={<Login />} />
             <Route path="/admin" element={<Admin />} />
+            <Route path="/articles/:id" element={<ArticleDetails />} />
           </Routes>
         </main>
         <Footer />
