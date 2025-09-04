ff520492d22f8575c2f8ad88c837e26074754e2a
karlito0oo <73117222+karlito0oo@users.noreply.github.com>
2025-09-05 04:17:02 +0800

    Update ArticleSeeder.php

--- backend/database/seeders/ArticleSeeder.php
+++ backend/database/seeders/ArticleSeeder.php
@@ -17,21 +17,21 @@ class ArticleSeeder extends Seeder
                 'title' => 'HAZARDOUS WASTE MANAGEMENT',
                 'description' => 'Hazardous waste poses a significant challenge when it comes to proper management.',
                 'content' => '<div><p>Hazardous waste poses a significant challenge when it comes to proper management. It is crucial to handle it in a way that prevents its detrimental effects on the environment and human health. Based on the guidelines and procedural manual of the DENR DAO92-29 on hazardous waste management and to ensure compliance with the EPR law, we have developed an end to end solution to assist waste generators in our country.</p><p>References: <a href="https://www.env.go.jp/en/recycle/asian_net/Country_Information/Law_N_Regulation/Philippines/DAO%202004-36.pdf">Procedural Manual Title lll DAO 92-29 "Hazardous Waste Management"</a> <a href="https://legacy.senate.gov.ph/republic_acts/ra%2011898.pdf">Republic Act No. 11898 EPR Law</a> <a href="https://www2.deloitte.com/ph/en/pages/risk/articles/epr-law-philippines.html">More info on EPR Law</a></p></div>',
-                'image' => '/articles/Screenshot_258.png',
+                'img_url' => '/articles/Screenshot_258.png',
                 'is_active' => true
             ],
             [
                 'title' => 'PHILIPPINE INTERNATIONAL FURNITURE SHOW',
                 'description' => 'WASTO & EcohomeArt showcased upcycled products from plastic waste and marine litter at the 2023 Philippine International Furniture Show.',
                 'content' => 'WASTO Waste Solutions & EcohomeArt also participated in the Philippine International Furniture Show 2023 from March 9-11 at SMX Convention Center Manila, Mall of Asia Complex in Pasay City to showcase the newly launched upcycled products from plastic waste materials & marine litter.',
-                'image' => '/articles/Screenshot_259.png',
+                'img_url' => '/articles/Screenshot_259.png',
                 'is_active' => true
             ],
             [
                 'title' => 'Eco Home Art Polyplastics',
                 'description' => 'POLYPLASTICS upcycles plastic waste into stylish, durable, and sustainable furniture, lighting, and accessories.',
                 'content' => 'POLYPLASTICS transforms rigid & flexible plastic waste into stylish and sustainable furniture, lighting, and accessories. By upcycling discarded plastics, we create innovative designs that merge durability with eco-conscious craftsmanship, giving waste a second life with purpose and style.',
-                'image' => '/articles/Screenshot_260.png',
+                'img_url' => '/articles/Screenshot_260.png',
                 'is_active' => true
             ]
         ];
